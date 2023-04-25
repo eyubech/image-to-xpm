6b13eb207c3fbd6f2dece9dfd8db1ce1726c3e18
Ayoub Ech-chetyouy <76597998+eyubech@users.noreply.github.com>
2023-04-26 00:37:01 +0100

    install mand model and add alias to zshrc file

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ from wand.image import Image
 
 argc = len(sys.argv)
 if (argc < 2):
-    print("Enter the image path after the command to_xpm example to_xpm ./image.png")
+    print("Please provide the path to the image file 'to_xpm' command.")
+    print("For example: 'to_xpm ./image.png'")
     exit
-
 if (argc > 2):
-    print("Too may arguments")
+    print("Too many arguments")
     exit
 
 
 img = sys.argv[1]
-
 output = ""
-
 for _ in img:
     if _ == '.': break
     output += _
 output += '.xpm'
 
+try:
+    with Image(filename=img) as img:
+        img.format = 'xpm'
+        xpm_data = img.make_blob(format='xpm')
+    with open(output, 'wb') as f:
+        f.write(xpm_data)
+except:
+    print("Please check the image path or the config file is executed.")
 
-with Image(filename=img) as img:
-    img.format = 'xpm'
-    xpm_data = img.make_blob(format='xpm')
-
-with open(output, 'wb') as f:
-    f.write(xpm_data)
